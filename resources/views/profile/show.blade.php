9af9b4ef5a9b74303a43944c444269ebc2220ec1
MAsad961 <pirsaien499@gmail.com>
2021-01-13 18:06:00 +0500

    update

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -12,7 +12,7 @@
 
                 <x-jet-section-border />
             @endif
-{{--
+
             @if (Laravel\Fortify\Features::enabled(Laravel\Fortify\Features::updatePasswords()))
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.update-password-form')
@@ -20,7 +20,7 @@
 
                 <x-jet-section-border />
             @endif
-
+{{--
             @if (Laravel\Fortify\Features::canManageTwoFactorAuthentication())
                 <div class="mt-10 sm:mt-0">
                     @livewire('profile.two-factor-authentication-form')
